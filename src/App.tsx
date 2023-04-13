3df0b9876a671ec1c5904704fb55abba526c7281
lucas <stempest@163.com>
2023-04-13 18:16:32 +0800

    拖拽思路实现

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React, {useReducer} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {FFmpegKitConfig} from 'ffmpeg-kit-react-native';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {initialState, reducer} from './views/reducer';
+import React, { useReducer } from "react";
+import { StyleSheet, View } from "react-native";
+import { FFmpegKitConfig } from "ffmpeg-kit-react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { initialState, reducer } from "./views/reducer";
 
 FFmpegKitConfig.disableLogs();
 
 // children
-import VideoView from './views/VideoPlayer';
-// import TimeAxios from './views/TimeAxios';
-import Controller from './views/Controller';
+import VideoView from "./views/VideoPlayer";
+import TimeAxios from "./views/TimeAxios";
+import Controller from "./views/Controller";
 // action
-import {SetThumbnailList, setVideoUri} from './views/action';
+import { SetThumbnailList, setVideoUri } from "./views/action";
 
 export default function App(): JSX.Element {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -20,8 +20,8 @@ export default function App(): JSX.Element {
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.container}>
         <VideoView uri={state.uri} poster="https://stempest0925.github.io/static/media/banner-cover.47f71d48.jpg" />
+        <TimeAxios thumbnailList={state.thumbnailList} />
       </View>
-      {/* <TimeAxios thumbnailList={state.thumbnailList} /> */}
       <Controller
         setVideoUri={(uri: string) => dispatch(setVideoUri(uri))}
         setThumbnailList={(list: string[]) => dispatch(SetThumbnailList(list))}
--- src/views/Controller.tsx
+++ src/views/Controller.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import {StyleSheet, View, Text, TouchableHighlight, Alert, Platform} from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, TouchableHighlight, Alert, Platform } from "react-native";
 // import {requestPermission} from '../helpers/permission';
 
-import RNFS from 'react-native-fs';
-import DocumentPicker from 'react-native-document-picker';
-import FFmepg from '../helpers/ffmpegCommand';
-import useProgressPopup from './ProgressPopup';
-import fileSystem from '../helpers/fileSystem';
-import {FFmpegKit} from 'ffmpeg-kit-react-native';
+import RNFS from "react-native-fs";
+import DocumentPicker from "react-native-document-picker";
+import FFmepg from "../helpers/ffmpegCommand";
+import useProgressPopup from "./ProgressPopup";
+import fileSystem from "../helpers/fileSystem";
+import { FFmpegKit } from "ffmpeg-kit-react-native";
 
-function getDir(defaultPath: 'cache' | 'document') {
-  const path = defaultPath === 'document' ? RNFS.DocumentDirectoryPath : RNFS.CachesDirectoryPath;
-  return Platform.OS === 'android' ? 'file://' + path : path;
+function getDir(defaultPath: "cache" | "document") {
+  const path = defaultPath === "document" ? RNFS.DocumentDirectoryPath : RNFS.CachesDirectoryPath;
+  return Platform.OS === "android" ? "file://" + path : path;
 }
 
 interface ControllerProps {
@@ -20,22 +20,22 @@ interface ControllerProps {
 }
 
 export default function Controller(props: ControllerProps): JSX.Element {
-  const {ProgressPopup, setProgress, openProgressPopup} = useProgressPopup({onCancel: () => {}});
+  const { ProgressPopup, setProgress, openProgressPopup } = useProgressPopup({ onCancel: () => {} });
 
   const btns = [
-    {title: '选择影片', onPress: () => pickVideo()},
-    {title: '查看缓存', onPress: () => {}},
+    { title: "选择影片", onPress: () => pickVideo() },
+    { title: "查看缓存", onPress: () => {} },
   ];
 
   const pickVideo = async () => {
     // await requestPermission('android.permission.READ_EXTERNAL_STORAGE');
     // fileSystem.clearCache('thumbnails').then(res => console.log('清理thumbnail: ', res));
 
-    await fileSystem.mkdir('videos', 'cache');
+    await fileSystem.mkdir("videos", "cache");
     // await fileSystem.mkdir('thumbnails', 'cache');
 
     const pickValue = await DocumentPicker.pickSingle({
-      copyTo: 'cachesDirectory',
+      copyTo: "cachesDirectory",
       type: DocumentPicker.types.video,
     });
 
@@ -44,7 +44,7 @@ export default function Controller(props: ControllerProps): JSX.Element {
       // const outFilePath = getDir('cache') + '/videos/video_' + new Date().getTime() + '.mp4';
       // videoTranscoding(pickValue.fileCopyUri, outFilePath);
     } else {
-      Alert.alert('选择视频出错');
+      Alert.alert("选择视频出错");
     }
   };
 
@@ -65,7 +65,7 @@ export default function Controller(props: ControllerProps): JSX.Element {
   };
 
   const getThumbnailList = (filePath: string) => {
-    const cacheDir = fileSystem.CACHE_ROOT_DIR + 'thumbnails/';
+    const cacheDir = fileSystem.CACHE_ROOT_DIR + "thumbnails/";
     const outImage = `${cacheDir}image_%02d.jpg`;
     FFmpegKit.execute(`-i ${filePath} -r 2 ${outImage}`).then(async () => {
       const readDir = await RNFS.readDir(cacheDir);
@@ -95,12 +95,12 @@ const styles = StyleSheet.create({
   btn: {
     padding: 12,
     marginTop: 12,
-    backgroundColor: '#3ddc84',
+    backgroundColor: "#3ddc84",
     borderRadius: 6,
   },
   btnText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
